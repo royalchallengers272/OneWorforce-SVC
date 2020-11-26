c451cf3e7a5f96c0d93dcc26857adf4d2a0573fc
Prinu <prinjohn@cisco.com>
2020-11-26 16:59:46 +0530

    First Commit

---
+++ oneworforce/src/main/java/com/learning/oneworforce/OneworforceApplication.java
@@ -0,0 +1,17 @@
+package com.learning.oneworforce;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class OneworforceApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneworforceApplication.class, args);
+	}
+
+}
